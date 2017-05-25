bf6ad8d17a30bd7ec0f5202d87e9345b378d9171
rmitdrdrone2017 <s3541094@student.rmit.edu.au>
2017-05-25 16:04:27 +1000

    Add files via upload

--- read-data.py
+++ read-data.py
@@ -1,14 +1,17 @@
 from pyMultiwii import MultiWii
-import time
+from sys import stdout
 
 if __name__ == "__main__":
 
     board = MultiWii("/dev/ttyUSB0")
+
     try:
-		board.getData(MultiWii.MOTOR)
-        message = "m1 = {:+.2f} \t m2 = {:+.2f} \t m3 = {:+.2f} \t m4 = {:+.4f} \t".format(float(board.motor['m1']),float(board.motor['m2']),float(board.motor['m3']),float(board.motor['m4']))
-        stdout.write("\r%s" % message )
-        stdout.flush()
+        while True:
+            board.getData(MultiWii.MOTOR)
+
+            message = "m1 = {:+.2f} \t m2 = {:+.2f} \t m3 = {:+.2f} \t m4 = {:+.4f} \t".format(float(board.motor['m1']),float(board.motor['m2']),float(board.motor['m3']),float(board.motor['m4']))
+            stdout.write("\r%s" % message )
+            stdout.flush()
 
     except Exception,error:
         print "Error on Main: "+str(error)
