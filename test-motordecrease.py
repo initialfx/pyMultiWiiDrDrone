d25ae86cbce7d59956170552e5370a6e7d243fba
rmitdrdrone2017 <s3541094@student.rmit.edu.au>
2017-05-25 15:42:48 +1000

    Add files via upload

---
+++ test-motordecrease.py
@@ -0,0 +1,11 @@
+from pyMultiwii import MultiWii
+import time
+
+if __name__ == "__main__":
+
+    board = MultiWii("/dev/ttyUSB0")
+    try:
+        data = data - [100, 100, 100, 100]
+
+    except Exception,error:
+        print "Error on Main: "+str(error)
